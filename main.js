3b85238d92e043c2f2b60eeb1499fae9d456f6a7
katyp03 <katyp03@gmail.com>
2017-06-14 13:55:44 -0400

    Fixed JS code and added background colors

--- main.js
+++ main.js
@@ -1,51 +1,42 @@
-// Attempt 1:
-// document.addEventListener("DOMContentLoaded", function(){
-// 	wine.addEventListener("click", function(){
-// 		var wine = document.getElementById("winecan");
-// 		var countWine = document.getElementById("count1");
-// 			countWine.innerText = parseInt(Cookies.get('wine')) || 0;
-// 	});
-// 	team.addEventListener("click", function(){
-// 		var team = document.getElementById("teamocil");
-// 		var countTeam = document.getElementById("count2");
-// 			countTeam.innerText = parseInt(Cookies.get('team')) || 0;
-// 	});
-// 	fight.addEventListener("click", function(){
-// 		var fight = document.getElementById("fightmilk");
-// 		var countFight = document.getElementById("count3");
-// 			countFight.innerText = parseInt(Cookies.get('fight')) || 0;
-// 	});
-// })
-
 // Attempt 2:
-var wineCount = parseInt(Cookies.get('WineCans')) || 0,
-	teamCount = parseInt(Cookies.get('TeamPills')) || 0,
-	fightCount = parseInt(Cookies.get('FightMilks')) || 0;
+// var wineCount = parseInt(Cookies.get('WineCans')) || 0,
+// 	teamCount = parseInt(Cookies.get('TeamPills')) || 0,
+// 	fightCount = parseInt(Cookies.get('FightMilks')) || 0;
 
-document.getElementById('count1').innerHTML = wineCount;
-document.getElementById('count2').innerHTML = teamCount;
-document.getElementById('count3').innerHTML = fightCount;
+// document.getElementById('count1').innerHTML = wineCount;
+// document.getElementById('count2').innerHTML = teamCount;
+// document.getElementById('count3').innerHTML = fightCount;
 
 document.addEventListener("DOMContentLoaded", function(){
-	document.querySelector("#winecan").onclick = function (){
+	wineCount = parseInt(Cookies.get('WineCans')) || 0,
+	teamCount = parseInt(Cookies.get('TeamPills')) || 0,
+	fightCount = parseInt(Cookies.get('FightMilks')) || 0;
+	var wine = document.getElementById('winecan');
+	var team = document.getElementById('teamocil');
+	var fight = document.getElementById('fightmilk');
+	var check = document.getElementById('checkout');
+	document.getElementById('count1').innerHTML = wineCount;
+	document.getElementById('count2').innerHTML = teamCount;
+	document.getElementById('count3').innerHTML = fightCount;
+	wine.addEventListener("click", function() {
 		wineCount++;
 	document.getElementById('count1').innerHTML = wineCount;
 	Cookies.set( 'WineCans', wineCount );
-	};
-	document.querySelector("#teamocil").onclick = function (){
+	});
+	team.addEventListener("click", function() {
 		teamCount++;
 	document.getElementById('count2').innerHTML = teamCount;
 	Cookies.set( 'TeamPills', teamCount );	
-	};
-	document.querySelector("#fightmilk").onclick = function (){
+	});
+	fight.addEventListener("click", function() {
 		fightCount++;
 	document.getElementById('count3').innerHTML = fightCount;
 	Cookies.set( 'FightMilks', fightCount );	
-	};
-	document.getElementById('checkout').onclick = function(){
+	});
+	check.addEventListener("click", function() {
 		Cookies.set( 'WineCans', 0 );
 		Cookies.set( 'TeamPills', 0 );
 		Cookies.set( 'FightMilks', 0 );
 		window.location.reload();
-	};
+	});
 });
